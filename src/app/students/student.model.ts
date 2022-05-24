320f26ef2bd77e0970a667122ddc41b2e2a8080f
OleksiiNikitenko <78477052+OleksiiNikitenko@users.noreply.github.com>
2022-05-24 23:20:02 +0300

    add student model and service

---
+++ src/app/students/student.model.ts
@@ -0,0 +1,26 @@
+export interface Student {
+  id: number;
+  name: string;
+  faculty: string;
+  studyYear: string;
+  grade: string;
+  groupName: string;
+  appRole: string;
+  email: string;
+  reserveEmail: string;
+  phone: string;
+  imgUrl: string;
+  studentCode: string;
+}
+
+// private Long id;
+// private String name;
+// private String faculty;
+// private String studyYear;
+// private String grade;
+// private String groupName;
+// private String appRole;
+// private String email;
+// private String reserveEmail;
+// private String phone;
+// private String imgUrl;
